f7a90a36ec49268a9ae2598085f269a8db4ef802
ProgrammerJayT <theanthem8@gmail.com>
2024-02-05 01:55:49 +0200

    Added user latitude and longitude attributes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role');
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
