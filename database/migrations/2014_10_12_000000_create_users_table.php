cb1f68f04543706104f656669c178c4ef4bd5981
ProgrammerJayT <theanthem8@gmail.com>
2024-02-05 01:43:43 +0200

    Added attributes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->integer('user_id')->autoIncrement();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('surname');
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role');
             $table->rememberToken();
             $table->timestamps();
         });
