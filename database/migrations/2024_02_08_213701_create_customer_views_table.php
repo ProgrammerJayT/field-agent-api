69fd3ad667890c298d5d04000cef860fb9753b19
ProgrammerJayT <theanthem8@gmail.com>
2024-02-09 01:51:19 +0200

    Created customer view model and migration

---
+++ database/migrations/2024_02_08_213701_create_customer_views_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customer_views', function (Blueprint $table) {
+            $table->integer('customer_view_id')->autoIncrement();
+            $table->integer('viewer_id');
+            $table->integer('customer_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customer_views');
+    }
+};
