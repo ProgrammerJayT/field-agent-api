962abcbd3b14697c5fb378c7cb68582a433e2433
ProgrammerJayT <theanthem8@gmail.com>
2024-02-09 01:51:39 +0200

    Added customers route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -33,6 +34,8 @@ Route::group(['prefix' => 'v1'], function () {
         Route::get('/verify-token', function () {
             return response()->json(['message' => 'Token is valid']);
         });
+
+        Route::apiResource('customers', CustomerController::class);
 
         Route::apiResource('users', UserController::class);
     });
