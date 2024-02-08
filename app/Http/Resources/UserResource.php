f27257536d07cee17155e7d9e6e819ae7bb4bf11
ProgrammerJayT <theanthem8@gmail.com>
2024-02-08 18:36:23 +0200

    Added json response attributes

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,16 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+
+        return [
+            'id' => $this->user_id,
+            'name' => $this->name,
+            'surname' => $this->surname,
+            'email' => $this->email,
+            'role' => $this->role,
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+        ];
     }
 }
