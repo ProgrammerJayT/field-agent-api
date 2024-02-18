3907671c174c60921749e7b22db49ae1c4753836
ProgrammerJayT <theanthem8@gmail.com>
2024-02-18 12:12:31 +0200

    Created view customer endpoint

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\CustomerViewController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -36,6 +37,8 @@ Route::group(['prefix' => 'v1'], function () {
         });
 
         Route::apiResource('customers', CustomerController::class);
+
+        Route::get('view-customer', [CustomerController::class, 'viewCustomer']);
 
         Route::apiResource('users', UserController::class);
     });
